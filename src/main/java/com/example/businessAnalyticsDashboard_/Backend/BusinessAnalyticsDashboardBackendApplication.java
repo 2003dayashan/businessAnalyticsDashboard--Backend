d87aa6eca82d331a993b8ec70774a2b7b93182dd
Chamuditha <2003dayashan@gmail.com>
2026-07-04 23:40:53 +0530

    main flies added

---
+++ src/main/java/com/example/businessAnalyticsDashboard_/Backend/BusinessAnalyticsDashboardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.businessAnalyticsDashboard_.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessAnalyticsDashboardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessAnalyticsDashboardBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/businessAnalyticsDashboard_/Backend/BusinessAnalyticsDashboardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.businessAnalyticsDashboard_.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessAnalyticsDashboardBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
